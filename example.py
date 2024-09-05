36ec60715941b1e768d9f56dd35cd8372dce3522
Facu <facuperezt@gmail.com>
2024-09-05 09:19:12 +0200

    train for 3, 4 and 5 kernels

--- example.py
+++ example.py
@@ -8,9 +8,9 @@ from src.trainers import TrainWithSyntheticData, TrainWithRealData
 def get_class_name(instance) -> str:
     return repr(instance.__class__).strip("'>").split(".")[-1]
 
-def train_with_synth_data(run_cfg: Dict[str, Any]):
+def train_with_synth_data(n_kernels: int, run_cfg: Dict[str, Any]):
     # model = VariationalAutoencoder("manual_simple_ae.json")
-    model = ResNetWeirdness(n_kernels=1, block_size=16)
+    model = ResNetWeirdness(n_kernels=n_kernels, block_size=16)
     # model.load_state_dict(torch.load("vae_synth_data.pth"))
     trainer = TrainWithSyntheticData(model, num_blocks=1500)
     try:
@@ -18,7 +18,7 @@ def train_with_synth_data(run_cfg: Dict[str, Any]):
     except Exception as e:
         print(e)
     finally:
-        torch.save(model.state_dict(), f"{get_class_name(model)}_synth_data.pth")
+        torch.save(model.state_dict(), f"{get_class_name(model)}_n_kernels_{n_kernels}_synth_data.pth")
 
 
 def finetune_with_real_data(run_cfg: Dict[str, Any]):
@@ -36,5 +36,6 @@ def finetune_with_real_data(run_cfg: Dict[str, Any]):
 if __name__ == "__main__":
     with open("src/trainers/configs/simple_training.json", "r") as f:
         cfg: Dict[str, Any] = json.load(f)
-    train_with_synth_data(run_cfg=cfg)
+    for i in range(3, 6):
+        train_with_synth_data(n_kernels=i, run_cfg=cfg)
     # finetune_with_real_data()
